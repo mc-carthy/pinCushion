eb6c47c2d0c1017584268a31163e648a0ca12f7d
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-12 11:13:57 +0100

    Create NeedleMovement

---
+++ Assets/scripts/needleScripts/NeedleMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeedleMovement : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject needleBody;
+
+	private bool canShootNeedle;
+	private bool touchedCustion;
+
+	private float speed = 70f;
+
+	private Rigidbody2D rb;
+
+	private void Awake () {
+		Initialize ();
+	}
+
+	private void Update () {
+		if (canShootNeedle) {
+			rb.velocity = new Vector2 (0, speed);
+		}
+	}
+
+	public void FireNeedle () {
+		needleBody.SetActive (true);
+		rb.isKinematic = false;
+		canShootNeedle = true;
+	}
+
+	private void Initialize () {
+		needleBody.SetActive (false);
+		rb = GetComponent<Rigidbody2D> ();
+	}
+}

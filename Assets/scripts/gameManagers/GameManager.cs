574ed4bbbffc8433e9835063905f7db3713d4cb2
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-12 11:37:14 +0100

    Create GameManager

---
+++ Assets/scripts/gameManagers/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+
+	[SerializeField]
+	private GameObject needle;
+	[SerializeField]
+	private int needleCount;
+
+	private Button shootBtn;
+	private GameObject[] needles;
+	private float needleDistance = 1.5f;
+	private int needleIndex;
+
+	private void Awake () {
+
+	}
+}
